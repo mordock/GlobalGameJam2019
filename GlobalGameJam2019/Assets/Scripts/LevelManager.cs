808dc2835bacb30794b2e497e0794ce12ecdb7d0
Max Weijers <maxweijers@hotmail.nl>
2019-01-26 00:06:27 +0100

    fix

---
+++ GlobalGameJam2019/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNewLevel(string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
